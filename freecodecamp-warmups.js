d5827eca114a3baad09d4ae4e9ee0beb0229a698
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-07-25 22:03:04 -0700

    factorialize

--- freecodecamp-warmups.js
+++ freecodecamp-warmups.js
@@ -3,3 +3,16 @@ function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
+function factorialize(num) {
+  if (num === 0) {
+   return 1;
+  }
+  var array = [];
+  for (var i = 1; i <= num; i++) {
+   array.push(i);
+  }
+  return array.reduce(function(x,y) {
+   return x * y;
+  })
+};
+
