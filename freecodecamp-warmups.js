5ebe5d10c2cb5713d036fdb025a9ea08706516f7
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-07-26 22:50:39 -0700

    largestOfFour

--- freecodecamp-warmups.js
+++ freecodecamp-warmups.js
@@ -50,3 +50,13 @@ function titleCase(str) {
   })
   return newArr.join(' ')
 };
+
+function largestOfFour(arr) {
+  var largest = [];
+  for (var i = 0; i < arr.length; i++) {
+    largest.push(Math.max.apply(null,arr[i]));
+  }
+  return largest;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
