58fc77b72a1633cab9920900599cdf02edb55be8
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-07-26 22:50:52 -0700

    confirmEnding

--- freecodecamp-warmups.js
+++ freecodecamp-warmups.js
@@ -60,3 +60,9 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+function confirmEnding(str, target) {
+  return (str.slice(str.length - target.length) === target);
+}
+
+confirmEnding("Bastian", "n");
