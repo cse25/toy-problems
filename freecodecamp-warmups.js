439020d6702d659e7d34ec87de785bc7881c4218
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-07-28 16:12:46 -0700

    truncateString

--- freecodecamp-warmups.js
+++ freecodecamp-warmups.js
@@ -73,4 +73,14 @@ confirmEnding("Bastian", "n");function repeatStringNumTimes(str, num) {
   return repeatedString;
 }
 
-repeatStringNumTimes('abc', 3); // Output: 'abcabcabc'
+repeatStringNumTimes('abc', 3); // Output: 'abcabcabc'function truncateString(str, num) {
+  if (num <= 3) {
+    return str.slice(0, num) + '...';
+  } else if (str.length > num) {
+    return str.slice(0, num - 3) + '...';
+  } else {
+    return str;
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
