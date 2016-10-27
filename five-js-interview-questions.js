638f74008b59873530dea6db7d994478604da3aa
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-10-26 18:10:36 -0700

    question 2

--- five-js-interview-questions.js
+++ five-js-interview-questions.js
@@ -15,3 +15,18 @@ console.log(b);
 // Answer: 5
 // Because 'b' is declared without the keyword var, it is assigned to the global scope.
 // Using strict mode would also require explicit reference to the global scope.
+
+// Question 2: Create “native” methods
+// Define a repeatify function on the String object. 
+// The function accepts an integer that specifies how many times the string has to be repeated. 
+// The function returns the string repeated the number of times specified. For example:
+
+String.prototype.repeatify = function(numberOfTimes) {
+  for (var i = 0; i <= numberOfTimes; i++) {
+    console.log(this);
+  }
+}
+
+console.log('hello'.repeatify(3));
+// Should print hellohellohello.
+
