ab0bd5c8cef86cd55215754bb9ff3523cdfa447f
Chris Csekey <cse25@users.noreply.github.com>
2017-05-07 20:39:57 -0700

    solve factorializeES6

---
+++ factorializeES6.js
@@ -0,0 +1,15 @@
+const factorialize = (num) => {
+  if (num < 0) {
+    return -1
+  }
+
+  else if (num === 0) {
+    return 1
+  }
+
+  else {
+    return (num * factorialize(num - 1))
+  }
+}
+
+factorialize(3)
