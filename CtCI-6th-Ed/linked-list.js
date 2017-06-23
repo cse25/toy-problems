a8b13b5436cc36e3613270197c51b7d69343d674
Chris Csekey <cse25@users.noreply.github.com>
2017-06-22 17:11:21 -0700

    implement addToTail method

--- CtCI-6th-Ed/linked-list.js
+++ CtCI-6th-Ed/linked-list.js
@@ -8,4 +8,20 @@ class LinkedList {
   getLength() {
     return this.count
   }
+  
+  addToTail(value) {
+    const node ={
+      value: value,
+      next: null
+    }
+    
+    if (this.count === 0) {
+      this.head = node
+    } else {
+      this.tail.next = node
+    }
+    
+    this.tail = node
+    this.count++
+  }
 }
